feb2f371638f043b43cc841ab18d034f799df749
Luiz Eduardo Rangel da Silva Abranches <luizeduardo.abranches@gmail.com>
2021-05-18 17:49:53 -0300

    Add files via upload

---
+++ ApiDIO/Api/Data/Collections/GeoJson2DGeographicCoordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Data.Collections
+{
+    internal class GeoJson2DGeographicCoordinates
+    {
+        private double longitude;
+        private double latitude;
+
+        public GeoJson2DGeographicCoordinates(double longitude, double latitude)
+        {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+    }
+}
